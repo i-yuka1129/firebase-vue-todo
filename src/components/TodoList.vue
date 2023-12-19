ccae8e21bc6625c95f3a7546e432223a8f17a096
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-12-19 15:26:10 +0900

    リストコンポーネントの作成

---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+<b-container>
+  <b-row>
+    <b-col cols="8">
+      <h5>テキスト</h5>
+    </b-col>
+    <b-col cols="4">
+      <b-button>完了</b-button>
+      <b-button>削除</b-button>
+    </b-col>
+  </b-row>
+</b-container>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+};
+</script>
+
+<style scoped>
+
+</style>
